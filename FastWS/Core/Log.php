4ae10c1d662404d909e5503716d76ab737694a86
lixuancn <lixuan868686@163.com>
2016-04-29 09:38:53 +0800

    修复QPS过多时会造成文件描述符用尽的bug

--- FastWS/Core/Log.php
+++ FastWS/Core/Log.php
@@ -10,8 +10,17 @@
 namespace FastWS\Core;
 
 class Log{
+    private static $fileResource = null;
+
+    private static function getInstance(){
+        if(is_null(self::$fileResource)){
+            self::$fileResource = fopen(FASTWS_LOG_PATH, 'a');
+        }
+    }
+
     public static function write($msg, $type='INFO')
     {
+        self::getInstance();
         $type = strtoupper($type);
         if(!in_array($type, array('INFO', 'ERROR', 'FATAL', 'WARNING', "TEST"))){
             exit('Log type no match');
@@ -20,7 +29,7 @@ class Log{
         if(FASTWS_DEBUG){
             echo $msg;
         }
-        file_put_contents(FASTWS_LOG_PATH, $msg, FILE_APPEND | LOCK_EX);
+        fwrite(self::$fileResource, $msg);
         if($type === 'FATAL'){
             exit;
         }
