f752db82b70230e3642e3854e93b8e7d6727ac9d
lixuancn <lixuan868686@163.com>
2016-05-20 10:22:07 +0800

    demo

--- demo-http-webserver.php
+++ demo-http-webserver.php
@@ -10,20 +10,16 @@
  */
 
 //引入FastWS
-include_once 'FastWS/index.php';
+require_once 'FastWS/index.php';
 
 //使用文本传输的Api类
-$webServer = new \FastWS\Api\WebServer('http', '0.0.0.0', '19910');
+$webServer = new \FastWS\Api\WebServer('0.0.0.0', '19910');
 
 //启动的子进程数量. 通常为CPU核心数
 $webServer->childProcessCount = 1;
 
 //设置FastWS实例名称
 $webServer->instanceName = 'FastWS-Http';
-
-//设置启动FastWS的用户和用户组
-$webServer->user = 'lane';
-$webServer->group = 'staff';
 
 //设置主
 $webServer->setRoot('www.lanecn.com', __DIR__.'/Test/Web');
