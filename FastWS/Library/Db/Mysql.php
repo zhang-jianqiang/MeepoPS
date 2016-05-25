420c910f706df75aacc3747ded1b3bcb2b757eed
lixuancn <lixuan868686@163.com>
2016-05-25 15:03:14 +0800

    文档

--- FastWS/Library/Db/Mysql.php
+++ FastWS/Library/Db/Mysql.php
@@ -9,25 +9,62 @@
  */
 namespace FastWS\Library\Db;
 
+use FastWS\Core\Log;
+
 class Mysql{
     public static $conn = null;
+
+    private $_host;
+    private $_username;
+    private $_password;
+    private $_port;
+    private $_dbName;
+
+    /**
+     * Mysql constructor.
+     * @param string $host
+     * @param string $username
+     * @param string $password
+     * @param string $dbName
+     * @param string $port
+     */
     public function __construct($host='', $username='', $password='', $dbName='', $port='3306')
     {
         if(is_null(self::$conn)){
-            $conn = mysqli_connect($host, $username, $password, $dbName, $port);
-            self::$conn = ($conn && is_object($conn)) ? $conn : null;
+            $this->_host = $host;
+            $this->_username = $username;
+            $this->_password = $password;
+            $this->_port = $port;
+            $this->_dbName = $dbName;
+            $this->_connect();
         }
     }
 
+    /**
+     * 执行Sql语句
+     * @param $sql
+     */
     public function query($sql){
-        return mysqli_query(self::$conn, $sql);
+        $result = mysqli_query(self::$conn, $sql);
+        if($result === false){
+            self::$conn = null;
+            $this->_connect();
+        }
+        return $result;
     }
 
-    public function add(){
-
-    }
-
-    public function select(){
-
+    /**
+     * 链接Mysql
+     */
+    private function _connect(){
+        while(is_null(self::$conn = null)){
+            self::$conn = mysqli_connect($this->_host, $this->_username, $this->_password, $this->_dbName, $this->_port);
+            if(self::$conn && is_object(self::$conn)){
+                break;
+            }
+            self::$conn = null;
+            Log::write(__METHOD__.' Mysql connect failed', 'ERROR');
+            sleep(10);
+        }
     }
 }
